121613af5d17f20d9961c510d9c6309d30e0a83c
johnowlee <johnowlee@gmail.com>
2024-09-17 13:04:06 +0900

    test: 재고 입고 유즈케이스 테스트 작성

--- src/test/java/com/shippingflow/core/usecase/aggregate/item/IncreaseStockUseCaseTest.java
+++ src/test/java/com/shippingflow/core/usecase/aggregate/item/IncreaseStockUseCaseTest.java
@@ -1,10 +1,12 @@
 package com.shippingflow.core.usecase.aggregate.item;
 
 import com.shippingflow.core.domain.aggregate.item.local.Stock;
+import com.shippingflow.core.domain.aggregate.item.local.StockTransactionType;
 import com.shippingflow.core.domain.aggregate.item.repository.ItemReaderRepository;
 import com.shippingflow.core.domain.aggregate.item.repository.ItemWriterRepository;
 import com.shippingflow.core.domain.aggregate.item.root.Item;
-import com.shippingflow.core.usecase.aggregate.item.vo.ItemVo;
+import com.shippingflow.core.exception.DomainException;
+import com.shippingflow.core.exception.error.ItemError;
 import com.shippingflow.core.usecase.common.ClockManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.BDDAssertions.tuple;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class IncreaseStockUseCaseTest {
@@ -38,24 +44,52 @@ class IncreaseStockUseCaseTest {
     @Test
     void execute_increaseStockQuantityAndRecordTransaction() {
         // given
+        Item item = Item.builder().id(1L).name("itemA").build();
         long itemId = 1L;
-        long quantity = 1000L;
-        Item item = Item.builder().id(itemId).build();
-        Stock stock = Stock.builder().id(1L).build();
-        item.bind(stock);
+        long quantityToAdd = 50L;
+        LocalDateTime transactionDateTime = LocalDateTime.now();
+        UpdateStockUseCase.Input input = UpdateStockUseCase.Input.of(itemId, quantityToAdd);
 
-        UpdateStockUseCase.Input input = UpdateStockUseCase.Input.of(1L, quantity);
+        Item increasedItem = Item.builder()
+                .id(item.getId())
+                .name(item.getName())
+                .build();
+        Stock stock = Stock.builder()
+                .quantity(quantityToAdd)
+                .build();
+        increasedItem.bind(stock);
+        increasedItem.recordStockTransaction(StockTransactionType.INCREASE, quantityToAdd, transactionDateTime);
 
-        given(itemReaderRepository.findById(input.getItemId())).willReturn(Optional.of(item));
-        given(itemWriterRepository.update(any(ItemVo.class))).willReturn(item);
+        given(itemReaderRepository.findById(itemId)).willReturn(Optional.of(item));
+        given(clockManager.getNowDateTime()).willReturn(transactionDateTime);
+        given(itemWriterRepository.update(increasedItem.toVo())).willReturn(increasedItem);
 
         // when
-        UpdateStockUseCase.Output result = increaseStockUseCase.execute(input);
+        UpdateStockUseCase.Output output = increaseStockUseCase.execute(input);
 
         // then
-        ItemVo actual = result.getItem();
-        assertThat(actual.id()).isEqualTo(itemId);
-        assertThat(actual.stock().quantity()).isEqualTo(quantity);
+        assertThat(output.getItem()).isNotNull();
+        assertThat(output.getItem().stock().quantity()).isEqualTo(50L);
+        assertThat(output.getItem().stock().transactions()).hasSize(1)
+                .extracting("transactionType", "quantity", "transactionDateTime")
+                .contains(
+                        tuple(StockTransactionType.INCREASE, quantityToAdd, transactionDateTime)
+                );
+        then(itemReaderRepository).should(times(1)).findById(itemId);
+        then(itemWriterRepository).should(times(1)).update(item.toVo());
     }
 
+    @DisplayName("상품 조회시 존재하지 않으면 예외가 발생한다.")
+    @Test
+    void execute_shouldThrowExceptionWhenItemIsNotFound() {
+        // given
+        UpdateStockUseCase.Input input = UpdateStockUseCase.Input.of(1L, 50L);
+
+        given(itemReaderRepository.findById(1L)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> increaseStockUseCase.execute(input))
+                        .isInstanceOf(DomainException.class)
+                        .hasMessage(ItemError.NOT_FOUND_ITEM.getMessage());
+    }
 }
