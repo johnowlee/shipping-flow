0290d6ec84488777ddc64fda00f5ad9583784fc6
johnowlee <johnowlee@gmail.com>
2024-09-23 00:29:20 +0900

    feat: UpdateStockUseCaseFactory 작성
    API에서 StockTransactionType 에 따라 UpdateStockUseCaseFactory에서 호출하여 유즈케이스 구현체 주입 요망

---
+++ src/main/java/com/shippingflow/core/usecase/aggregate/item/UpdateStockUseCaseFactory.java
@@ -0,0 +1,25 @@
+package com.shippingflow.core.usecase.aggregate.item;
+
+import com.shippingflow.core.domain.aggregate.item.model.local.StockTransactionType;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UpdateStockUseCaseFactory {
+
+    private final Map<StockTransactionType, UpdateStockUseCase> strategies = new HashMap<>();;
+
+    public UpdateStockUseCaseFactory(List<UpdateStockUseCase> useCases) {
+        useCases.forEach(useCase -> {
+            StockTransactionType type = useCase.getTransactionType();
+            strategies.put(type, useCase);
+        });
+    }
+
+    public UpdateStockUseCase getUseCase(StockTransactionType transactionType) {
+        return strategies.get(transactionType);
+    }
+}
